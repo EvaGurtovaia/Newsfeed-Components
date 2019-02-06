fc8c761ff28bc5ae9aa2d717089af23a3111b1dd
EvaGurtovaia <eva.gurtovaya@gmail.com>
2019-02-05 19:59:24 -0600

    Updated

--- Article/Article.js
+++ Article/Article.js
@@ -1,64 +1,41 @@
-// Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+// Because classes are not hoised you will need to start your code at the bottom of the page. 
+// Look for the comment "START HERE"
 
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector ('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.innerText = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
   }
 
+  
   expandArticle() {
+    this.domElement.classList.toggle('article-open');
+    console.log('xo-xo'); 
     // Using our reference to the domElement, toggle a class to expand or hide the article.
 
   }
 }
 
+
 /* START HERE: 
 
 - Select all classes named ".article" and assign that value to the articles variable.  
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the constructor.
+- With your selection in place, now chain .forEach() on to the articles variable to
+ iterate over the articles NodeList and create a new instance of Article 
+ by passing in each article as a parameter to the constructor.
 
 */
 
-let articles = document.querySelectorAll('.articles');
+let articles = document.querySelectorAll('.article');
 articles.forEach(something => new Article(something));
 
 
 
 
-//class Panel {
- // constructor(newPanel) {
-   // this.panelButtons = newPanel.querySelector('.panel-buttons');
-    // select the panel-buttons
-    // find the open-close buttons and assign them to this.
-    //this.panelOpenBtn = newPanel.querySelector('.panel-btn-open');
-    //this.panelCloseBtn = newPanel.querySelector('.panel-btn-close');
-    //this.panelContent = newPanel.querySelector('.panel-content');
-    // add some eventListener to panelsButtons
-    // we need to toggle classes to elements nested within the pane-buttons div
-    // this.panelButtons.addEventListener('click', this.toggle.bind(this));
-    ///this.panelButtons.addEventListener('click', this.toggle);
-  //}
-  
-  //toggle = () => {
-   // console.log(this.panelOpenBtn);
-    //this.panelOpenBtn.classList.toggle('hide-btn');
-    //this.panelCloseBtn.classList.toggle('hide-btn');
-   // this.panelContent.classList.toggle('panel-content');
- // }
-  
-//}
-
-//const panels = document.querySelectorAll('.panel');
-// select all elements w/ class .panel and store them in a list.
-
-// loop over new NodeList (panels) generate new Panel from scratch.
-//panels.forEach(panel => new Panel(panel));
-// for each element in our NodeList, create a new Class for DOM Manipulation.
-// w/ no parens for an arrow function, there is an IMPLICIT Return.
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,35 +9,5 @@ const menu = document.querySelector ('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', )
+menuButton.addEventListener('click', toggleMenu);
 
-//class Panel {
- // constructor(newPanel) {
-   // this.panelButtons = newPanel.querySelector('.panel-buttons');
-    // select the panel-buttons
-    // find the open-close buttons and assign them to this.
-    //this.panelOpenBtn = newPanel.querySelector('.panel-btn-open');
-    //this.panelCloseBtn = newPanel.querySelector('.panel-btn-close');
-    //this.panelContent = newPanel.querySelector('.panel-content');
-    // add some eventListener to panelsButtons
-    // we need to toggle classes to elements nested within the pane-buttons div
-    // this.panelButtons.addEventListener('click', this.toggle.bind(this));
-    ///this.panelButtons.addEventListener('click', this.toggle);
-  //}
-  
-  //toggle = () => {
-   // console.log(this.panelOpenBtn);
-    //this.panelOpenBtn.classList.toggle('hide-btn');
-    //this.panelCloseBtn.classList.toggle('hide-btn');
-   // this.panelContent.classList.toggle('panel-content');
- // }
-  
-//}
-
-//const panels = document.querySelectorAll('.panel');
-// select all elements w/ class .panel and store them in a list.
-
-// loop over new NodeList (panels) generate new Panel from scratch.
-//panels.forEach(panel => new Panel(panel));
-// for each element in our NodeList, create a new Class for DOM Manipulation.
-// w/ no parens for an arrow function, there is an IMPLICIT Return.
